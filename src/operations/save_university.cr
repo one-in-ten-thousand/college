12537bc356a76ab8021dac79ccc0ac546a83d323
Billy.Zheng <vil963@gmail.com>
2024-04-25 12:23:14 +0800

    Update operations for validate and before_save

--- src/operations/save_university.cr
+++ src/operations/save_university.cr
@@ -1,6 +1,21 @@
 class SaveUniversity < University::SaveOperation
-  # To save user provided params to the database, you must permit them
-  # https://luckyframework.org/guides/database/saving-records#perma-permitting-columns
-  #
-  # permit_columns column_1, column_2
+  upsert_lookup_columns name
+  permit_columns name, description, code, is_211, is_985, is_good
+
+  attribute province_name : String
+  attribute city_name : String
+
+  before_save do
+    validate_required name
+    validate_required province_name
+    validate_required city_name
+
+    validate_uniqueness_of name
+
+    province = SaveProvince.upsert!(name: province_name.value.to_s)
+    city = SaveCity.upsert!(name: city_name.value.to_s, province_id: province.id)
+
+    province_id.value = province.id
+    city_id.value = city.id
+  end
 end
