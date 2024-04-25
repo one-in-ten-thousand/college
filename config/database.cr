07886aa9df2bfdcf75c29414979035ad64b82a39
Billy.Zheng <vil963@gmail.com>
2024-04-25 18:43:58 +0800

    Add validate i18n

--- config/database.cr
+++ config/database.cr
@@ -19,6 +19,27 @@ AppDatabase.configure do |settings|
   p! settings.credentials.url
 end
 
+struct MyI18n < Avram::I18nBackend
+  def get(key : String | Symbol) : String
+    {
+      validate_acceptance_of:      "must be accepted",
+      validate_at_most_one_filled: "must be blank",
+      validate_confirmation_of:    "必须匹配",
+      validate_exact_size_of:      "长度必须为 %d 个字符",
+      validate_exactly_one_filled: "at least one must be filled",
+      validate_format_of:          "is invalid",
+      validate_inclusion_of:       "is not included in the list",
+      validate_max_size_of:        "must not have more than %d characters",
+      validate_min_size_of:        "must have at least %d characters",
+      validate_numeric_max:        "must be no more than %s",
+      validate_numeric_min:        "must be at least %s",
+      validate_numeric_nil:        "数字不可以为空",
+      validate_required:           "需要的",
+      validate_uniqueness_of:      "已经存在了",
+    }[key]
+  end
+end
+
 Avram.configure do |settings|
   settings.database_to_migrate = AppDatabase
 
@@ -29,6 +50,7 @@ Avram.configure do |settings|
   # Always parse `Time` values with these specific formats.
   # Used for both database values, and datetime input fields.
   # settings.time_formats << "%F"
+  settings.i18n_backend = MyI18n.new
 end
 
 if !LuckyEnv.task?
