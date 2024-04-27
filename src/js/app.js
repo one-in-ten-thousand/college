5fe5151c8c294f7a4c590c1afc6771e00b112820
Billy.Zheng <vil963@gmail.com>
2024-04-27 17:15:18 +0800

    修复了从 materializecss 引入 M ,终于知道 import M 和 import {M } 的区别了.

--- src/js/app.js
+++ src/js/app.js
@@ -4,40 +4,46 @@
 // Though it says "Rails" it actually works with any framework.
 import Rails from "@rails/ujs";
 Rails.start();
-import M from '@materializecss/materialize';
+import { M } from '@materializecss/materialize';
 import { iosProvinces, iosCitys } from './areaData_v2.js';
 import IosSelect from "iosselect";
 import 'htmx.org';
 window.htmx = require('htmx.org');
 
-document.getElementById('select_address')?.addEventListener('click', function () {
-    var showAddress = document.getElementById('show_address');
-    var provinceCode = document.getElementById('province-code');
-    var provinceName = document.getElementById('province-name');
-    var cityCode = document.getElementById('city-code');
-    var cityName = document.getElementById('city-name');
-    var oneLevelId = showAddress.getAttribute('data-province-code');
-    var twoLevelId = showAddress.getAttribute('data-city-code');
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('select_address')?.addEventListener('click', function () {
+        var showAddress = document.getElementById('show_address');
+        var provinceCode = document.getElementById('province-code');
+        var provinceName = document.getElementById('province-name');
+        var cityCode = document.getElementById('city-code');
+        var cityName = document.getElementById('city-name');
+        var oneLevelId = showAddress.getAttribute('data-province-code');
+        var twoLevelId = showAddress.getAttribute('data-city-code');
 
-    var iosSelect = new IosSelect(2,
-        [iosProvinces, iosCitys],
-        {
-            title: '地址选择',
-            itemHeight: 35,
-            relation: [1, 1],
-            oneLevelId: oneLevelId,
-            twoLevelId: twoLevelId,
-            callback: function (selectOneObj, selectTwoObj) {
-                provinceCode.value = selectOneObj.id
-                provinceName.value = selectOneObj.value
+        var iosSelect = new IosSelect(2,
+            [iosProvinces, iosCitys],
+            {
+                title: '地址选择',
+                itemHeight: 35,
+                relation: [1, 1],
+                oneLevelId: oneLevelId,
+                twoLevelId: twoLevelId,
+                callback: function (selectOneObj, selectTwoObj) {
+                    provinceCode.value = selectOneObj.id
+                    provinceName.value = selectOneObj.value
 
-                cityCode.value = selectTwoObj.id
-                cityName.value = selectTwoObj.value
+                    cityCode.value = selectTwoObj.id
+                    cityName.value = selectTwoObj.value
 
-                showAddress.setAttribute('data-province-code', selectOneObj.id);
-                showAddress.setAttribute('data-city-code', selectTwoObj.id);
-                showAddress.innerHTML = '点击选择省、市：    ' + selectOneObj.value + ' ' + selectTwoObj.value;
-            }
-        });
+                    showAddress.setAttribute('data-province-code', selectOneObj.id);
+                    showAddress.setAttribute('data-city-code', selectTwoObj.id);
+                    showAddress.innerHTML = '点击选择省、市：    ' + selectOneObj.value + ' ' + selectTwoObj.value;
+                }
+            });
+    });
+
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, {
+        // specify options here
+    });
 });
-//
