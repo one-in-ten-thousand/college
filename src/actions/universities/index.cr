81e2b8119d6d01bbeaa164989e878b30604f5810
Billy.Zheng <vil963@gmail.com>
2024-04-29 11:03:20 +0800

    每页显示 50 条

--- src/actions/universities/index.cr
+++ src/actions/universities/index.cr
@@ -1,6 +1,6 @@
 class Universities::Index < BrowserAction
   get "/universities" do
-    pages, universities = paginate(UniversityQuery.new.id.asc_order)
+    pages, universities = paginate(UniversityQuery.new.id.asc_order, per_page: 50)
     html IndexPage, universities: universities, pages: pages
   end
 end
