6663fbd52f128a22eb9f9c111dacbc69232d2537
Billy.Zheng <vil963@gmail.com>
2024-05-01 15:41:46 +0800

    Search on description too.

--- src/actions/universities/index.cr
+++ src/actions/universities/index.cr
@@ -3,10 +3,16 @@ class Universities::Index < BrowserAction
     q = params.get?(:q)
 
     builder = Avram::QueryBuilder.new(table: :universities)
-    builder = builder.where(Avram::Where::Raw.new("name &@~ ?", q)) unless q.blank?
-    query = UniversityQuery.new
-    query.query = builder
-    pages, universities = paginate(query.id.desc_order, per_page: 50)
+    unless q.blank?
+      builder = builder.where(Avram::Where::Raw.new("name &@~ ?", q))
+      builder = builder.or do |or|
+        or.where(Avram::Where::Raw.new("description &@~ ?", q))
+      end
+    end
+    results = UniversityQuery.new
+    results.query = builder
+
+    pages, universities = paginate(results.id.desc_order, per_page: 50)
 
     html IndexPage, universities: universities, pages: pages
   end
