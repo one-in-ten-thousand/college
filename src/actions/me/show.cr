15e389c0b7e6a8c96d9a18b33fe42a7625786a0f
Billy.Zheng <vil963@gmail.com>
2024-05-29 16:31:26 +0800

    User can edit only itself except zw963

--- src/actions/me/show.cr
+++ src/actions/me/show.cr
@@ -2,6 +2,11 @@ class Me::Show < BrowserAction
   get "/me" do
     users = UserQuery.new.id.desc_order
 
+    if current_user.email != "zw963@163.com"
+      users = users.id(current_user.id)
+    end
+
+
     html ShowPage, users: users
   end
 end
