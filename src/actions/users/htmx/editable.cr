a3e1d11270df664443a568d833b83ffbef3e59b6
Billy.Zheng <vil963@gmail.com>
2024-05-30 18:15:22 +0800

    only zw963 editable.

--- src/actions/users/htmx/editable.cr
+++ src/actions/users/htmx/editable.cr
@@ -2,7 +2,9 @@ class User::Htmx::Editable < HtmxAction
   put "/users/:id/editable" do
     user = UserQuery.find(params.get(:id))
 
-    SaveUser.update!(user, is_editable: !user.is_editable)
+    if current_user.email == "zw963@163.com"
+      SaveUser.update!(user, is_editable: !user.is_editable)
+    end
 
     plain_text "ok"
   end
