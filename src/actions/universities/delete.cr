df8de11bc4d5411c8524ed1f21387884cb729550
Billy.Zheng <vil963@gmail.com>
2024-06-01 10:26:57 +0800

    user can't delete university.

--- src/actions/universities/delete.cr
+++ src/actions/universities/delete.cr
@@ -1,9 +1,13 @@
 class Universities::Delete < BrowserAction
   delete "/universities/:university_id" do
-    university = UniversityQuery.find(university_id)
-    DeleteUniversity.delete(university) do |_operation, _deleted|
-      flash.success = "删除成功"
-      redirect Index
+    if current_user.email == "zw963@163.com"
+      university = UniversityQuery.find(university_id)
+      DeleteUniversity.delete(university) do |_operation, _deleted|
+        flash.success = "删除成功"
+        redirect Index
+      end
+    else
+      plain_text "不被允许的"
     end
   end
 end
