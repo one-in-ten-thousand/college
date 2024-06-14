2ecdb5fd749bf7663bc81b7010f91fb4e009c85d
Billy.Zheng <vil963@gmail.com>
2024-06-14 14:39:22 +0800

    Fix edit menu bug.

--- src/components/universities/name_link.cr
+++ src/components/universities/name_link.cr
@@ -15,7 +15,7 @@ class Universities::NameLink < BaseComponent
       marked_2021: university.marked_2021(current_user),
       marked_2020: university.marked_2020(current_user),
       marked: university.marked(current_user),
-      script: "on click set @href of <ul#dropdown3 li a[href='data_edit_url']/> to '#{Edit.with(university).path}'
+      script: "on click set @href of <ul#dropdown3 li a/> to '#{Edit.with(university).path}'
 then set @hx-put of <ul#dropdown3 li input[name='university:is_marked']/> to '#{Universities::Htmx::Marked.with(university.id).path}'
 then set @hx-target of <ul#dropdown3 li input[name='university:is_marked']/> to '##{id}'
 then set @hx-target of <ul#dropdown3 li input[name='university:is_marked_2023']/> to '##{id}'
