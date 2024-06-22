9d80d7b4d642363079f41a1f62f9e6f3172b68aa
Billy.Zheng <vil963@gmail.com>
2024-06-22 17:37:34 +0800

    Workaround is_excluded feature.

--- src/actions/universities/htmx/excluded.cr
+++ src/actions/universities/htmx/excluded.cr
@@ -7,13 +7,12 @@ class Universities::Htmx::Excluded < HtmxAction
 
     chong_wen_bao = SaveChongWenBao.create!(user_id: user_id, university_id: university.id) if chong_wen_bao.nil?
 
-    SaveChongWenBao.update!(chong_wen_bao, params)
+    # SaveChongWenBao.update!(chong_wen_bao, is_excluded: params.get_raw(:"chong_wen_bao:is_excluded") == "true" ? true : false)
+    SaveChongWenBao.update!(chong_wen_bao, is_excluded: !chong_wen_bao.is_excluded)
 
     case request.headers["HX-Trigger"]?
     when "excluded"
       component NameLink, university: university.reload(&.preload_chong_wen_baos), current_user: current_user
-    when /unexcluded/
-      plain_text ""
     else
       plain_text "ok"
     end
