9e272adebeb612aff7ff7981f85955f4093d8229
Billy.Zheng <vil963@gmail.com>
2024-06-24 17:31:16 +0800

    Add more info

--- src/actions/mixins/page_helpers.cr
+++ src/actions/mixins/page_helpers.cr
@@ -15,17 +15,17 @@ module PageHelpers
           score_offset = (university.score_2023_min.not_nil!.to_i - base_score_2023) - (university.score_2022_min.not_nil!.to_i - base_score_2022)
         end
       when 2022
-        base_score = 498
+        base_score = base_score_2022
         unless university.score_2022_min.nil? || university.score_2021_min.nil?
           score_offset = (university.score_2022_min.not_nil!.to_i - base_score_2022) - (university.score_2021_min.not_nil!.to_i - base_score_2021)
         end
       when 2021
-        base_score = 505
+        base_score = base_score_2021
         unless university.score_2021_min.nil? || university.score_2020_min.nil?
           score_offset = (university.score_2021_min.not_nil!.to_i - base_score_2021) - (university.score_2020_min.not_nil!.to_i - base_score_2020)
         end
       when 2020
-        base_score = 537
+        base_score = base_score_2020
       end
 
       unless base_score.nil?
@@ -72,18 +72,52 @@ module PageHelpers
         unless university.ranking_2023_min.nil? || university.ranking_2022_min.nil?
           ranking_offset = university.ranking_2023_min.not_nil!.to_i - university.ranking_2022_min.not_nil!.to_i
         end
+
+        base_ranking_min = 42972.42458
+
+        unless university.ranking_2023_min.nil?
+          current_ranking_offset = university.ranking_2023_min.not_nil!.to_i - base_ranking_min.to_i
+        end
       when 2022
         unless university.ranking_2022_min.nil? || university.ranking_2021_min.nil?
           ranking_offset = university.ranking_2022_min.not_nil!.to_i - university.ranking_2021_min.not_nil!.to_i
+        end
+        base_ranking_min = 35905.35321
+
+        unless university.ranking_2022_min.nil?
+          current_ranking_offset = university.ranking_2022_min.not_nil!.to_i - base_ranking_min.to_i
         end
       when 2021
         unless university.ranking_2021_min.nil? || university.ranking_2020_min.nil?
           ranking_offset = university.ranking_2021_min.not_nil!.to_i - university.ranking_2020_min.not_nil!.to_i
         end
+        base_ranking_min = 34766.34225
+
+        unless university.ranking_2021_min.nil?
+          current_ranking_offset = university.ranking_2021_min.not_nil!.to_i - base_ranking_min.to_i
+        end
+      when 2020
+        base_ranking_min = 32871.32357
+
+        unless university.ranking_2020_min.nil?
+          current_ranking_offset = university.ranking_2020_min.not_nil!.to_i - base_ranking_min.to_i
+        end
       end
 
       str = String.build do |io|
         io << "同分段 #{same_ranking_count} 人"
+
+        unless current_ranking_offset.nil?
+          io << "\n"
+
+          if current_ranking_offset >= 0
+            io << "投档线超过一本线"
+          else
+            io << "投档线低于一本线"
+          end
+
+          io << " #{current_ranking_offset.abs} 位"
+        end
 
         unless ranking_offset.nil?
           io << "\n位次比上一年"
@@ -96,6 +130,8 @@ module PageHelpers
           io << ranking_offset.abs
           io << " 位"
         end
+
+        io << "\n\n备注: 一本线对应位次: #{base_ranking_min}"
       end
 
       str
