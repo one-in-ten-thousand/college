43021a393850d2b10ed2ff6fdb3767b05dedd71c
Billy.Zheng <vil963@gmail.com>
2024-06-24 18:52:58 +0800

    includes search_all by default

--- src/actions/universities/index.cr
+++ src/actions/universities/index.cr
@@ -91,7 +91,7 @@ class Universities::Index < BrowserAction
       "q", "is_985", "is_211", "is_good", "is_exists_remark", "is_marked",
       "is_marked_2023", "is_marked_2022", "is_marked_2021", "is_marked_2020",
       "order_by", "batch_level", "filter_by_column", "cwb_union_set",
-      "range_min_value", "range_max_value",
+      "range_min_value", "range_max_value", "search_all",
       "chong_2023", "chong_2022", "chong_2021", "chong_2020",
       "wen_2023", "wen_2022", "wen_2021", "wen_2020",
       "bao_2023", "bao_2022", "bao_2021", "bao_2020",
